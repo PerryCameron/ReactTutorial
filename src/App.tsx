877a80fbc5e9320d2f8e4e1eeef6011569bc67e0
PerryCameron <pcameron28@yahoo.com>
2023-09-22 14:07:25 -0400

    1:16:34

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/alert";
+import { useState } from "react";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  const [alertVisable, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisable && <Alert>Hello World </Alert>}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
