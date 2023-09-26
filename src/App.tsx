06cbe1643e04eb63796147afcda08dc7b24ff434
PerryCameron <pcameron28@yahoo.com>
2023-09-26 19:45:16 -0400

    First part of mini project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ];
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      />
     </div>
   );
 }
