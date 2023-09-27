8bb6991e8e99c23f0ff7177c81aafcae16ff8228
PerryCameron <pcameron28@yahoo.com>
2023-09-27 11:29:07 -0400

    Can now select categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 20, category: "Utilities" },
@@ -10,10 +12,19 @@ function App() {
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
