2f824403344d8c07b58c98df7c24edfcc49d8d06
PerryCameron <pcameron28@yahoo.com>
2023-09-27 10:59:30 -0400

    Expenses will now delete

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import Form from "./components/Form";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 20, category: "Utilities" },
     { id: 3, description: "ccc", amount: 10, category: "Utilities" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ];
+  ]);
+
   return (
     <div>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("Delete", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
