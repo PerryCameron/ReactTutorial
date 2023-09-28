560f8404cb360a478d0b774b1a169af352fe470e
PerryCameron <pcameron28@yahoo.com>
2023-09-27 20:08:41 -0400

    validation now works

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"] as const;
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -18,6 +21,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
