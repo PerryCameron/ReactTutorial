ca8115a9581df84cf61a638b8652e770f9590654
PerryCameron <pcameron28@yahoo.com>
2023-09-28 08:19:35 -0400

    added error messages

--- src/components/expense-tracker/components/ExpenseForm.tsx
+++ src/components/expense-tracker/components/ExpenseForm.tsx
@@ -4,9 +4,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import categories from "../categories";
 
 const schema = z.object({
-  description: z.string().min(3).max(50),
-  amount: z.number().min(0.01).max(100_000),
-  category: z.enum(categories),
+  description: z
+    .string()
+    .min(3, { message: "Description should be at lease 3 characters" })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount is required." })
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required." }),
+  }),
 });
 
 type ExpenseFormData = z.infer<typeof schema>;
